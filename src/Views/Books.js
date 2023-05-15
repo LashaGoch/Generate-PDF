182f35d78ba684a431b2977af1773738c8d10190
LashaGoch <47014691+LashaGoch@users.noreply.github.com>
2023-05-15 04:14:43 +0200

    Create Books.js

---
+++ src/Views/Books.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Books() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">Books page</h1>
+      <p>This is the BOOKS.</p>
+    </div>
+  );
+}
+
+export default Books;
