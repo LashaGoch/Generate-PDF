6eceb9e8bb1ed0ee77155fbbcc0d632d2fee6055
LashaGoch <47014691+LashaGoch@users.noreply.github.com>
2023-05-15 04:15:37 +0200

    Create HowItWorks.js

---
+++ src/Views/HowItWorks.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HowItWorks() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">How it works</h1>
+      <p>This is the HOW IT WORKS.</p>
+    </div>
+  );
+}
+
+export default HowItWorks;
