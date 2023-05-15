67dfa4c30d11529388cc9819dfe0eea81daa56f3
LashaGoch <47014691+LashaGoch@users.noreply.github.com>
2023-05-15 04:15:00 +0200

    Create Contact.js

---
+++ src/Views/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">Contact Us</h1>
+      <p>This is the Contact US.</p>
+    </div>
+  );
+}
+
+export default Contact;
